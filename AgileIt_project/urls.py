3f9845e3e572ba2f486b6cfd11607cfa34b25453
JoshThould <jhethould@gmail.com>
2025-07-31 14:23:46 +0100

    Add URL routing for sprints app and create homepage view

--- AgileIt_project/urls.py
+++ AgileIt_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', include('sprints.urls')),  # Homepage and sprint views
+    # path('epics/', include('epics.urls')),     # Optional future expansion
+    # path('users/', include('users.urls')),     # For auth/profile routes
 ]
--- sprints/views.py
+++ sprints/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "sprints/homepage.html"
 
 # Create your views here.
