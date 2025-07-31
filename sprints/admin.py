8bc685976522797adacddf4e8a1d6332188b634f
JoshThould <jhethould@gmail.com>
2025-07-31 14:22:34 +0100

    Add Django admin for improved workflow

--- sprints/admin.py
+++ sprints/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Sprint, Epic, Story, Task, AcceptanceCriteria
 
 # Register your models here.
+
+admin.site.register(Sprint)
+admin.site.register(Epic)
+admin.site.register(Story)
+admin.site.register(Task)
+admin.site.register(AcceptanceCriteria)
