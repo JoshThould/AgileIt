a44fff83b2371d996624136bdabc8fdaf678cf6c
JoshThould <jhethould@gmail.com>
2025-08-04 12:35:50 +0100

    Add views and templates for sprint management including list, create, detail, update, and delete functionalities

--- sprints/views.py
+++ sprints/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Sprint
+
+# Home view for the AgileIT application
 
 class HomeView(TemplateView):
     template_name = "sprints/homepage.html"
 
-# Create your views here.
+# List all sprints
+
+class SprintListView(ListView):
+    model = Sprint
+    template_name = 'sprints/sprint_list.html'
+    context_object_name = 'sprints'
+
+# Create a new sprint
+class SprintCreateView(CreateView):
+    model = Sprint
+    fields = ['title', 'start_date', 'end_date']
+    template_name = 'sprints/sprint_form.html'
+    success_url = reverse_lazy('sprints:sprint-list')
+
+# View sprint details
+class SprintDetailView(DetailView):
+    model = Sprint
+    template_name = 'sprints/sprint_detail.html'
+    context_object_name = 'sprint'
+
+# Edit an existing sprint
+class SprintUpdateView(UpdateView):
+    model = Sprint
+    fields = ['title', 'start_date', 'end_date']
+    template_name = 'sprints/sprint_form.html'
+    success_url = reverse_lazy('sprints:sprint-list')
+
+# Delete a sprint
+class SprintDeleteView(DeleteView):
+    model = Sprint
+    template_name = 'sprints/sprint_confirm_delete.html'
+    success_url = reverse_lazy('sprints:sprint-list')
