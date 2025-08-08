351143118920d7bf5c3e8a40eaf45e0bfd2aa4c8
JoshThould <jhethould@gmail.com>
2025-08-08 12:41:52 +0100

    Refactor Story views: filter stories by owner in StoryListView and auto-assign owner in StoryCreateView

--- sprints/views.py
+++ sprints/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -10,7 +9,7 @@ from django.views.generic import (
     DeleteView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Sprint, Story, 
+from .models import Sprint, Story
 
 # Dashboard view with Kanban functionality
 class DashboardView(LoginRequiredMixin, TemplateView):
@@ -22,7 +21,7 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context['stories_in_progress'] = Story.objects.filter(status='In Progress', owner=self.request.user)
         context['stories_done'] = Story.objects.filter(status='Done', owner=self.request.user)
         return context
-    
+
 # Kanban board view
 class KanbanBoardView(LoginRequiredMixin, TemplateView):
     template_name = 'sprints/kanban.html'
@@ -37,66 +36,60 @@ class KanbanBoardView(LoginRequiredMixin, TemplateView):
         return context
 
 # Sprints Views
-# List all sprints
 class SprintListView(LoginRequiredMixin, ListView):
     model = Sprint
     template_name = 'sprints/sprint_list.html'
     context_object_name = 'sprints'
 
-# Create a new sprint
 class SprintCreateView(LoginRequiredMixin, CreateView):
     model = Sprint
     fields = ['title', 'start_date', 'end_date']
     template_name = 'sprints/sprint_form.html'
     success_url = reverse_lazy('sprints:sprint-list')
 
-# View sprint details
 class SprintDetailView(LoginRequiredMixin, DetailView):
     model = Sprint
     template_name = 'sprints/sprint_detail.html'
     context_object_name = 'sprint'
 
-# Edit an existing sprint
 class SprintUpdateView(LoginRequiredMixin, UpdateView):
     model = Sprint
     fields = ['title', 'start_date', 'end_date']
     template_name = 'sprints/sprint_form.html'
     success_url = reverse_lazy('sprints:sprint-list')
 
-# Delete a sprint
 class SprintDeleteView(LoginRequiredMixin, DeleteView):
     model = Sprint
     template_name = 'sprints/sprint_confirm_delete.html'
     success_url = reverse_lazy('sprints:sprint-list')
 
 # Story Views
-# List all Stories
 class StoryListView(LoginRequiredMixin, ListView):
     model = Story
     template_name = 'sprints/story_list.html'
     context_object_name = 'stories'
 
-# Create a new Story
+    def get_queryset(self):
+        return Story.objects.filter(owner=self.request.user)
+
 class StoryCreateView(LoginRequiredMixin, CreateView):
     model = Story
-    fields = ['title', 'description', 'status']
+    fields = ['title', 'description', 'status', 'sprint']  # exclude 'owner'
     template_name = 'sprints/story_form.html'
     success_url = reverse_lazy('sprints:story-list')
 
     def form_valid(self, form):
-        form.instance.owner = self.request.user
+        form.instance.owner = self.request.user  # auto-assign owner
         return super().form_valid(form)
 
-# View Story details
 class StoryDetailView(LoginRequiredMixin, DetailView):
     model = Story
     template_name = 'sprints/story_detail.html'
     context_object_name = 'story'
 
-# Edit an existing Story
 class StoryUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Story
-    fields = ['title', 'description', 'status']
+    fields = ['title', 'description', 'status', 'sprint']  # include 'sprint'
     template_name = 'sprints/story_form.html'
     success_url = reverse_lazy('sprints:story-list')
 
@@ -104,7 +97,6 @@ class StoryUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         story = self.get_object()
         return story.owner == self.request.user
 
-# Delete a Story
 class StoryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Story
     template_name = 'sprints/story_confirm_delete.html'
@@ -113,4 +105,3 @@ class StoryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         story = self.get_object()
         return story.owner == self.request.user
-
