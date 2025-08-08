d30f134f6292fea2314444cfb9da4eee9de8b13b
JoshThould <jhethould@gmail.com>
2025-08-08 12:15:30 +0100

    Refactor signals.py: remove unused Task and AcceptanceCriteria signal handlers; add logging for Story deletions

--- sprints/signals.py
+++ sprints/signals.py
@@ -1,14 +1,7 @@
-from .models import Story, Task, AcceptanceCriteria
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from .models import Task, AcceptanceCriteria
+from .models import Story
 
-@receiver([post_save, post_delete], sender=Task)
-def update_story_status_from_task(sender, instance, **kwargs):
-    if instance.story:
-        instance.story.update_status()
-
-@receiver([post_save, post_delete], sender=AcceptanceCriteria)
-def update_story_status_from_criteria(sender, instance, **kwargs):
-    if instance.story:
-        instance.story.update_status()
+@receiver(post_delete, sender=Story)
+def log_story_deletion(sender, instance, **kwargs):
+    print(f"[Audit] Story deleted: '{instance.title}' (ID: {instance.id})")
