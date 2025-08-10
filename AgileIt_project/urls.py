7cc994407110be72268313ac2261679783bfd1e7
JoshThould <jhethould@gmail.com>
2025-08-10 23:16:39 +0100

    Refactor URL configuration: streamline urlpatterns and enhance redirect handling

--- AgileIt_project/urls.py
+++ AgileIt_project/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url=reverse_lazy('sprints:dashboard'), permanent=False)),
+    path('', include(('sprints.urls', 'sprints'), namespace='sprints')),
     path('accounts/', include('allauth.urls')),
-    path('sprints/', include('sprints.urls')), 
+    path('admin/', admin.site.urls),
 ]
