e1d8acc76860de658b83355b66a50a72af9a6402
JoshThould <jhethould@gmail.com>
2025-08-11 22:35:39 +0100

    Update AllAuth settings: adjust login and logout redirect URLs, and modify authentication methods for improved user flow

--- AgileIt_project/settings.py
+++ AgileIt_project/settings.py
@@ -46,10 +46,9 @@ INSTALLED_APPS = [
 
 # AllAuth configuration
 
-SITE_ID = 1 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/dashboard/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -113,13 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # AllAuth settings
+LOGIN_REDIRECT_URL = '/dashboard/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
 
-LOGIN_REDIRECT_URL = '/homepage/'  # Redirects users after successful login
-ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'  # Redirects users after logout
-ACCOUNT_LOGIN_METHODS = {'username', 'email'}
-ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
-ACCOUNT_EMAIL_VERIFICATION = 'optional'  # Allows login without email confirmation
-
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
