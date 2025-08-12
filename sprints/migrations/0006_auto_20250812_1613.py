0bc4f3959bec5c96425b029053a3cd431356e238
JoshThould <jhethould@gmail.com>
2025-08-12 16:18:00 +0100

    Update migrations: add sprint field to story and modify owner field

---
+++ sprints/migrations/0006_auto_20250812_1613.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('sprints', '0004_rename_user_sprint_owner'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='story',
+            name='owner',
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
